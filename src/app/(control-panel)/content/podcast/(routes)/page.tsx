6c65b0cbf1a40980e43beda26976ba250f7e3224
lajmia69 <lajmiahmed175@gmail.com>
2026-04-23 18:38:09 +0100

    n

--- src/app/(control-panel)/content/podcast/(routes)/page.tsx
+++ src/app/(control-panel)/content/podcast/(routes)/page.tsx
@@ -1,7 +1,12 @@
 import { redirect } from 'next/navigation';
 
+<<<<<<< HEAD
 function AcademyApp() {
 	redirect(`/apps/academy/courses`);
+=======
+function PodcastApp() {
+	redirect(`/content/podcast`);
+>>>>>>> parent of 99c7bff (f)
 	return null;
 }
 
