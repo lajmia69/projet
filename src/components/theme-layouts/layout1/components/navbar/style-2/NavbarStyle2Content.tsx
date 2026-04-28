491f704dc2de4627fe166c61963105552cfa4045
lajmia69 <lajmiahmed175@gmail.com>
2026-04-28 14:25:21 +0100

    a

--- src/components/theme-layouts/layout1/components/navbar/style-2/NavbarStyle2Content.tsx
+++ src/components/theme-layouts/layout1/components/navbar/style-2/NavbarStyle2Content.tsx
@@ -109,17 +109,11 @@ function NavbarStyle2Content(props: NavbarStyle2ContentProps) {
 			<Root className={clsx('flex h-full flex-auto', className)}>
 				<div
 					id="fuse-navbar-side-panel"
-					className="flex h-full shrink-0 flex-col items-center"
+					className="flex h-full shrink-0 flex-col items-center justify-center"
 				>
-					<div className="flex flex-col items-center gap-2 my-4">
-						<img
-							className="h-30 w-30"
-							src="/assets/images/logo/logo.png"
-							alt="logo"
-						/>
-						<Typography className="tracking-light text-lg leading-none font-semibold text-center text-white">
-							EDUVOICE
-						</Typography>
+					<div className="flex flex-col items-center gap-2 mb-4">
+						<img className="h-30 w-30" src="/assets/images/logo/logo.png" alt="logo" />
+						<Typography className="text-white text-center font-semibold text-lg">EDUVOICE</Typography>
 					</div>
 
 					<FuseScrollbars
